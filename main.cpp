cf2c25d1e483c683ce33770359a87cd43403b839
minhtet97 <minhtetnai97@gmail.com>
2021-11-22 10:06:35 +0900

    test
    test

--- main.cpp
+++ main.cpp
@@ -1,65 +1,79 @@
 #include <stdio.h>
 
-class Array
+class Things
 {
-    
     private:
-        int num;
-        int* shinArray;
-
-
-    public:
-        Array()
-        {
-            shinArray = nullptr;
-        };
-        ~Array()
-        {
-            delete[] shinArray;
-            shinArray = nullptr;
-            printf("%p\n", shinArray);
-        }
-        void Create(int z)
-        {
-            shinArray = new int[z];
-            num = z;
-        }
-        int Get(int getz)
-        {
-            if (getz >= 0 && getx < num)
-                return shinArray[getz];
-            else
-                return NULL;
-        }
-
-        void Set(int setz)
-        {
-            if (setz >= 0 && setz < num)
-                newArray[setz] = setz;
-            else
-                return;
-        }
-
+        float a, b, c;
     
-
+    public:
+        Things()
+    
+    {
+            a = 0;
+            b = 0;
+            c = 0;
+    }
+    
+    float getA() { return a;}
+    float getB() { return b;}
+    float getC() { return c;}
+    
+    virtual void Update() {};
+        
 };
 
-int main()
+
+Class Go : public Things
 {
-    Array array;
-    array.Create(1000);
-
-    for (int i = 0; i < 1000; i++)
-    {
-        array.Set(i);
-    }
-
-    for (int i = 0; i < 1000; i++)
-    {
-        printf("num = %d\n", array.Get(i));
-    }
-
+    private
+    float Speed, Action;
     
-    "num = %d\n", array.Get(-1);
-    "num = %d\n", array.Get(1000);
+    
+    public
+    Go()
+    {
+        Speed = 0;
+        Action = 0;
+    }
+    
+    void setSpeed(float Speed, float Action);
+    float getSpeed() { return Speed;}
+    float getAction() { return accel;}
+    
+    virtual void Update(){}
+    
+}
+
+class Car : public Go
+{
+    public
+    
+    Go go;
+    void setSpeed(float Speed, float Action){ Go.setSpeed(Speed, Action)};
+    void Update();
+}
+
+void Go::setSpeed(float Speed, float Action)
+{
+    this->Speed = Speed;
+    this->Action = Action;
+}
+
+void Car::Update()
+{
+    float Speed;
+    Speed = Go.getSpeed();
+    printf("%f\n", Speed);
+}
+
+int main(){
+    
+    Car car;
+    car.setSpeed(10, 1);
+    
+    for (int i = 0; i < 100; i++)
+    {
+        car.Update();
+    }
+    return 0;
 }
